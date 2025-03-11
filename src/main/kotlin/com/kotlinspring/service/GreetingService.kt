e2ca0b27c8fb84dc57885aa6f4600459ae91b6bc
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-11 22:53:43 +0900

    [Service] yml파일을 이용한 프로필 셋업 테스트

--- src/main/kotlin/com/kotlinspring/service/GreetingService.kt
+++ src/main/kotlin/com/kotlinspring/service/GreetingService.kt
@@ -1,9 +1,16 @@
 package com.kotlinspring.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingService {
+    
+    // yaml파일의 message의 값을 가져옴
+    @Value("\${message}")
+    // 나중에 사용할 것이라고 선언
+    // 이후 값을 지정하면 그 값이 들어감
+    lateinit var message: String
 
-    fun retrieveGreeting(name:String) = "Hello, $name!"
+    fun retrieveGreeting(name:String) = "$name, $message"
 }
