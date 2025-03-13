614be985609eeb4bab2703c5663fb34df0e82019
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-13 21:06:05 +0900

    [Service] CourseService에 Course 생성 서비스 로직 작성

---
+++ src/main/kotlin/com/kotlinspring/service/CourseService.kt
@@ -0,0 +1,30 @@
+package com.kotlinspring.service
+
+import com.kotlinspring.dto.CourseDto
+import com.kotlinspring.entity.Course
+import com.kotlinspring.repository.CourseRepository
+import mu.KLogging
+import org.springframework.stereotype.Service
+
+@Service
+class CourseService(
+    val courseRepository: CourseRepository
+) {
+
+    companion object : KLogging()
+
+    // 코스 저장 서비스 로직
+    fun addCourse(courseDto: CourseDto) : CourseDto {
+        val courseEntity = courseDto.let {
+            Course(null, it.name, it.category)
+        }
+        
+        courseRepository.save(courseEntity)
+        logger.info("저장된 course : $courseEntity")
+
+
+        return courseEntity.let {
+            CourseDto(it.id, it.name, it.category)
+        }
+    }
+}
