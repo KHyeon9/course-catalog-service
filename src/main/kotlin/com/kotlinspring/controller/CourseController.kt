0168d12a271d5e7319a9d21bfdda6d6eb217ba60
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-13 21:05:39 +0900

    [Controller] CourseController에 Course 생성 코드 작성

---
+++ src/main/kotlin/com/kotlinspring/controller/CourseController.kt
@@ -0,0 +1,17 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.dto.CourseDto
+import com.kotlinspring.service.CourseService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/v1/courses")
+class CourseController(val courseService : CourseService) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // 요청 성공시 201 create 상태 반환
+    fun addCourse(@RequestBody courseDto: CourseDto) : CourseDto {
+        return courseService.addCourse(courseDto)
+    }
+}
