c5a9ab4d0c80479edeed58332b2fd3360d769b4e
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-14 20:00:22 +0900

    [Controller] Course 수정 및 삭제 코드 구현

--- src/main/kotlin/com/kotlinspring/controller/CourseController.kt
+++ src/main/kotlin/com/kotlinspring/controller/CourseController.kt
@@ -3,18 +3,34 @@ package com.kotlinspring.controller
 import com.kotlinspring.dto.CourseDto
 import com.kotlinspring.service.CourseService
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/v1/courses")
 class CourseController(val courseService : CourseService) {
 
+    // 코스 생성
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) // 요청 성공시 201 create 상태 반환
     fun addCourse(@RequestBody courseDto: CourseDto) : CourseDto {
         return courseService.addCourse(courseDto)
     }
 
+    // 모든 코스 조회
     @GetMapping
     fun retrieveAllCourses() : List<CourseDto> = courseService.retrieveCourses()
+
+    // 코스 수정
+    @PutMapping("/{course_id}")
+    fun updateCourse(
+        @PathVariable("course_id") courseId: Int,
+        @RequestBody courseDto: CourseDto
+    ) = courseService.updateCourse(courseId, courseDto)
+
+    // 코스 삭제
+    @DeleteMapping("/{course_id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 콘텐츠 없음 상태를 반환
+    fun deleteCourse(@PathVariable("course_id") courseId: Int)
+    = courseService.deleteCourse(courseId)
 }
