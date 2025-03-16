8c996c66e5ee47c45672f32ce435c82624f84def
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-16 19:54:45 +0900

    [Repository] course name에 값이 포함되면 조회하는 로직 추가 및 native query를 통해서 같은 로직 작성

--- src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -1,9 +1,17 @@
 package com.kotlinspring.repository
 
 import com.kotlinspring.entity.Course
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CourseRepository : CrudRepository<Course, Int> {
+    // 코스 네임에 인수가 포함된 코스가 존재하는지 확인하는 함수
+    fun findByNameContaining(courseName: String): List<Course>
+
+    // 위의 jpa 출력을 네이티브 쿼리로 작성함
+    @Query(value = "SELECT * FROM COURSES WHERE name LIKE %:courseName%", nativeQuery = true)
+    fun findCoursesByName(@Param("courseName") courseName: String): List<Course>
 }
