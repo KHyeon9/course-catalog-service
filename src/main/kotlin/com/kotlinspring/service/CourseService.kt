42f5759344b9adad780cd246e6942f2858cff416
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-18 02:36:49 +0900

    [Service] 강사 조회 및 강사가 없는 경우 커스텀 에러가 발생하도록 연결 및 entity, dto 수정으로 인한 입력 변수 수정

--- src/main/kotlin/com/kotlinspring/service/CourseService.kt
+++ src/main/kotlin/com/kotlinspring/service/CourseService.kt
@@ -3,28 +3,36 @@ package com.kotlinspring.service
 import com.kotlinspring.dto.CourseDto
 import com.kotlinspring.entity.Course
 import com.kotlinspring.exception.CourseNotFoundException
+import com.kotlinspring.exception.InstructorNotValidException
 import com.kotlinspring.repository.CourseRepository
 import mu.KLogging
 import org.springframework.stereotype.Service
 
 @Service
 class CourseService(
-    val courseRepository: CourseRepository
+    val courseRepository: CourseRepository,
+    private val instructorService: InstructorService
 ) {
 
     companion object : KLogging()
 
     // 코스 저장 서비스 로직
     fun addCourse(courseDto: CourseDto) : CourseDto {
+        val instructorOptional = instructorService.findByInstructorId(courseDto.instructorId!!)
+
+        if (!instructorOptional.isPresent) {
+            throw InstructorNotValidException("${courseDto.instructorId}에 해당하는 Instructor를 찾지 못했습니다.")
+        }
+
         val courseEntity = courseDto.let {
-            Course(null, it.name, it.category)
+            Course(null, it.name, it.category, instructorOptional.get())
         }
         
         courseRepository.save(courseEntity)
         logger.info("저장된 course : $courseEntity")
 
         return courseEntity.let {
-            CourseDto(it.id, it.name, it.category)
+            CourseDto(it.id, it.name, it.category, it.instructor!!.id)
         }
     }
 
