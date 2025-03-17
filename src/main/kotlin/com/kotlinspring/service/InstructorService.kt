7dd4ac221883d6861c0310218350f0a51323bbb4
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-18 02:32:19 +0900

    [Service] 강사 조회 메소드를 InstructorService에 추가

--- src/main/kotlin/com/kotlinspring/service/InstructorService.kt
+++ src/main/kotlin/com/kotlinspring/service/InstructorService.kt
@@ -4,6 +4,7 @@ import com.kotlinspring.dto.InstructorDto
 import com.kotlinspring.entity.Instructor
 import com.kotlinspring.repository.InstructorRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class InstructorService(
@@ -19,6 +20,11 @@ class InstructorService(
         return instructorEntity.let {
             InstructorDto(it.id, it.name)
         }
+    }
+
+    // 강사를 조회하는 로직
+    fun findByInstructorId(instructorId: Int): Optional<Instructor> {
+        return instructorRepository.findById(instructorId)
     }
 
 }
